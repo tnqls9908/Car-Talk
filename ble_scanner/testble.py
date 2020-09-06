9af7096de1d114554a031672d8364d35e293fa2e
JunYoungYoon <jjun9288@gmail.com>
2020-09-06 19:15:28 +0900

    testing code for scanning

---
+++ ble_scanner/testble.py
@@ -0,0 +1,23 @@
+# test BLE Scanning software
+
+import blescanner
+import sys
+
+import bluetooth._bluetooth as bluez
+
+dev_id = 0
+try:
+    sock = bluez.hci_open_dev(dev_id)
+    print("ble thread started")
+except:
+    print("error accessing bluetooth device...")
+    sys.exit(1)
+
+blescanner.hci_le_set_scan_parameters(sock)
+blescanner.hci_enable_le_scan(sock)
+
+while True:
+    returnedList = blescanner.parse_events(sock, 10)
+    print("----------")
+    for beacon in returnedList:
+        print(beacon)
