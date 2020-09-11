5af8440c62c0790d65c6141ce77eda1fb1bb6e64
chea-young <study.lchy@gmail.com>
2020-09-11 15:11:33 +0900

    ADD button.py cheayoung

---
+++ button.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO   
+import time
+RED = 18
+GREEN = 15
+YELLOW = 23
+GPIO.setmode(GPIO.BCM)      
+GPIO.setup(RED, GPIO.IN)
+GPIO.setup(GREEN, GPIO.IN)
+GPIO.setup(YELLOW, GPIO.IN)
+try:
+    while True:
+        if GPIO.input(GREEN)==1:
+            print ("Green Button was Pressed!" )
+        elif GPIO.input(RED)==1:
+            print ("Red Button was Pressed!" )  
+        elif GPIO.input(YELLOW)==1:
+            print ("Yellow Button was Pressed!" )                                                                                       
+        else:
+            print ("Button was Not Pressed!")
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+
